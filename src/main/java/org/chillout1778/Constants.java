af4aa4086c0508abc77cc397c60a3cd3b63d65b0
liyuhao <liyuhao20001118@gmail.com>
2025-07-13 17:23:33 +0800

    fix: update camera translation

--- src/main/java/org/chillout1778/Constants.java
+++ src/main/java/org/chillout1778/Constants.java
@@ -98,11 +98,11 @@ public class Constants {
             new Rotation3d(0.0, Math.toRadians(-30.0), Math.toRadians(-(180.0 - 60.0))));
     public static final Transform3d FRONT_LEFT_TRANSFORM =
         new Transform3d(
-            new Translation3d(0.016429, 0.324519, 0.18735),
+            new Translation3d(-0.016429, 0.324519, 0.18735),
             new Rotation3d(0.0, Math.toRadians(-30.0), Math.toRadians(60.0)));
     public static final Transform3d BACK_LEFT_TRANSFORM =
         new Transform3d(
-            new Translation3d(0.079749, 0.324875, 0.187354),
+            new Translation3d(-0.079749, 0.324875, 0.187354),
             new Rotation3d(0.0, Math.toRadians(-30.0), Math.toRadians(180.0 - 60.0)));
   }
 
